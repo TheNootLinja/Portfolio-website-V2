ec0284b9bac12af2cc69bf720c4b8024bfaf4077
TheNootLinja <thenootlinja@gmail.com>
2021-06-29 22:35:33 -0400

    Deleted/removed anything in the starter app that I didn't need

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
+const Header = () => (
+  <header
+    style={{
+      height: `55px`,
+      width: `100%`,
+      background: `#2A2730`
+    }}
+  >
+    <div>
+      <h1>NP</h1>
+    </div>
+  </header>
+)
+
+export default Header;
