bd0c8624b9c6cae5524417e62c9af35e93473603
TheNootLinja <thenootlinja@gmail.com>
2021-07-11 20:53:03 -0400

    Add start of styling from design sketch to the BlogPost component

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -1,10 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "./BlogPost.styles.css"
+
 const BlogPost = ({ post }) => (
-    <div>
+    <div className="blogpost">
         <Link to={post.frontmatter.slug}>
-            {post.frontmatter.title} ({post.frontmatter.date})
+            <h2 className="blogpost-title">{post.frontmatter.title}</h2>
+            <p className="blogpost-date">{post.frontmatter.date}</p>
+            <p>{post.excerpt}</p>
         </Link>
     </div>
 )
